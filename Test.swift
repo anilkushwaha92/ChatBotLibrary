cbe52f5788bccb43ca35d77099b902a886132a23
Anil Kushwaha <anil@appypiellp.com>
2021-05-03 12:30:18 +0530

    New file added in ChatBotLibrary

---
+++ Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  ChatBotLibrary
+//
+//  Created by Anil on 03/05/21.
+//
+
+import Foundation
+import FirebaseCore
+
+public class Test {
+    public init(){
+        
+    }
+    public func doSomething(){
+        print("Do something")
+    }
+}
