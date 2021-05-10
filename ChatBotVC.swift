51796090bb1af6e2fc935ea06d05bc26a50b5d2d
Anil Kushwaha <anil@appypiellp.com>
2021-05-10 17:37:49 +0530

    added as a public

--- ChatBotVC.swift
+++ ChatBotVC.swift
@@ -7,23 +7,20 @@
 
 import UIKit
 
-class ChatBotVC: UIViewController {
+public class ChatBotVC: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public init() {
+        super.init(nibName: "ChatBotVC", bundle: Bundle(for: ChatBotVC.self))
     }
-    */
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
